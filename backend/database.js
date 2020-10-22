237dfe6593ebeb0e80a7334bff675ccd16f3adcb
SaatwikRishi <38259107+SaatwikRishi@users.noreply.github.com>
2020-10-22 21:55:45 +0530

    LoginCheck function done

--- backend/database.js
+++ backend/database.js
@@ -1,24 +1,40 @@
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+
 var myobj = { name: "Henry", password : "123456", email: "henry@gmail.com", contacts: [{name: "Jassi", email: '123@gmail.com', phonenum: '12345678'}]};
 function dbconnect() {
     MongoClient.connect(url, function(err, db) {
+        
+        
         if (err) console.log(err);
         var dbo = db.db("assignment");
         console.log("Database created!");
-        dbo.collection('users').insertOne(myobj, function(err, res) {
-           if(err) throw err;
-           console.log(res); 
-        })
+
+
+        // dbo.collection('users').insertOne(myobj, function(err, res) {
+        //    if(err) throw err;
+        //    console.log(res); 
+        // })
     });
       
 }
-dbconnect();
 
 
+//dbconnect();
+logincheck('henry@gmail.com', "123")
+function logincheck(e, p) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) console.log(err);
+        var dbo = db.db("assignment");
+        //console.log("Database created!");
+        var query = {email: e, password: p};
+        dbo.collection('users').findOne(query, function(err, res) {
+            if(err) throw "Invalid Username or Password"
+            return "Login Successful"
+    });
 
-
+}) }
 
 
 
